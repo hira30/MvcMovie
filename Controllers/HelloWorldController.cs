c2b9caff0338590cc3109451c3d5d529806ddcac
hira30 <th11282@gmail.com>
2022-07-23 08:06:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // /HelloWorld にアクセスした時の処理
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // /HelloWorld/Welcome にアクセスした時の処理
+        public string Welcome(string name, int age = 1)
+        {
+            return $"名前：{name}さん, 年齢：{age}歳";
+        }
+    }
+}
